da2ae2758946390a0e5bf7cce79232bfc37e1886
TraigClark <traig.clark2@nov.com>
2024-07-03 14:08:07 -0500

    fully working docker.compose

--- hello-world/src/internal/mqttfile/mqtt.go
+++ hello-world/src/internal/mqttfile/mqtt.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"main/internal/configpkg"
 	"time"
+	"os"
+	"fmt"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -11,17 +13,23 @@ import (
 //copy - pasted from original main.go
 
 func MqttInit(config *configpkg.Config) mqtt.Client {
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker(broker)
-	opts.SetClientID(clientID)
-	mqttClient := mqtt.NewClient(opts)
-	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
-		if token.Error() != nil {
-			log.Println(token.Error())
-		}
-	}
+	broker := os.Getenv("MQTT_BROKER_ADDRESS")
+    if broker == "" {
+        fmt.Println("MQTT_BROKER_ADDRESS environment variable is not set")
+    }
 
-	return mqttClient
+    opts := mqtt.NewClientOptions().AddBroker(fmt.Sprintf("tcp://%s", broker))
+    opts.SetClientID("go_mqtt_client")
+
+    client := mqtt.NewClient(opts)
+    if token := client.Connect(); token.Wait() && token.Error() != nil {
+        log.Println(token.Error())
+    }
+
+    log.Println("Connected to MQTT broker")
+	
+	return client
+    // Your MQTT client logic here
 }
 
 func Publish(client mqtt.Client, topic string, payload string) {
